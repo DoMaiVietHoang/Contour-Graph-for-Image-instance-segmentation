6e9330c11c5f206cc476108b0fced14f9051139e
Admin <hoang.dmv200237@sis.hust.edu.vn>
2025-02-22 16:02:49 +0700

    GCN

--- GCN.py
+++ GCN.py
@@ -1,1 +1,21 @@
 import torch
+import torch.nn as nn
+from torch_geometric.nn import GCNConv
+from torch_geometric.data import Data
+class ContourMerge(nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers):
+        super(ContourMerge, self).__init__()
+        self.convs = nn.ModuleList()
+        self.convs.append(GCNConv(in_channels, hidden_channels))
+        for __ in range(num_layers - 2):
+            self.convs.append(GCNConv(hidden_channels, hidden_channels))
+        self.convs.append(GCNConv(hidden_channels, out_channels))
+        self.mlp = nn.Sequential(
+            torch.nn.Linear(out_channels,64),
+            torch.nn.ReLU(),
+            torch.nn.Linear(64,2),
+        )
+    def forward(self, x, edge_index):
+        for conv in self.convs[:-1]:
+            x = conv(x, edge_index)
+            x = torch.relu(x)
